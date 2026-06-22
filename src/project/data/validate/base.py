17a05ec4e0f42f0166521be0d990ce03592f03c7
szapp <dev.szapp@gmail.com>
2026-06-22 21:30:08 +0200

    Move data model type coercion into checks

--- src/project/data/validate/base.py
+++ src/project/data/validate/base.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Any, Self
+from typing import Any
 
 import pandas as pd
 import pandera.pandas as pa
@@ -14,8 +14,14 @@ class DataModelBase(pa.DataFrameModel):
     index_: pat.Index[int] = F(unique=True, ge=0)  # DataFrame index
 
     class Config:
-        strict = "filter"  # Exclude extra columns
+        strict = "filter"  # Drop extra columns
         coerce = True  # Auto-convert data types where possible
+
+    @pa.dataframe_parser
+    def rename_columns(cls, df: pd.DataFrame) -> pd.DataFrame:
+        """Rename columns of input prior to validation."""
+        cls.get_logger().debug("Adjust column names")
+        return df.rename(columns=cls._pre_rename)
 
     @pa.check("^.*[^_]$", regex=True, ignore_na=False)
     def has_at_least_one_value(cls, col: pat.Series[Any]) -> bool:
@@ -26,7 +32,7 @@ class DataModelBase(pa.DataFrameModel):
     def index_is_monotonically_increasing(cls, idx: pat.Index[int]) -> bool:
         """Index must be monotonically increasing to reduce risk of mistakes."""
         cls.get_logger().debug("Check for proper index")
-        return idx.tolist() == pd.RangeIndex(len(idx)).tolist()
+        return idx.tolist() == list(range(len(idx)))
 
     @pa.dataframe_check
     def dataframe_is_non_empty(cls, df: pd.DataFrame) -> bool:
@@ -35,32 +41,20 @@ class DataModelBase(pa.DataFrameModel):
         return not df.empty
 
     @classmethod
-    def validate(
-        cls: type[Self], check_obj: pd.DataFrame, *args, **kwargs
-    ) -> pat.DataFrame[pa.DataFrameModel]:
-        cls.get_logger().debug("Adjust column names")
-        check_obj = check_obj.rename(columns=cls._pre_rename)
-        return super().validate(check_obj, *args, **kwargs)
-
-    @classmethod
     def get_logger(cls) -> logging.Logger:
         return logging.getLogger(f"{cls.__module__}.{cls.__qualname__}")
 
 
 class DataModelBaseML(DataModelBase):
-    """Data model base enforcing ML conform data types after validation."""
+    """Data model base enforcing ML conform data types after validation.
 
-    @classmethod
-    def validate(
-        cls: type[Self], check_obj: pd.DataFrame, *args, **kwargs
-    ) -> pat.DataFrame[pa.DataFrameModel]:
-        df = super().validate(check_obj, *args, **kwargs)
+    Allowed data types to provide a deterministic and reproducible ML context are
+    Float64, Boolean, Categorical, Datetime.
+    """
 
-        # Allowed data types to provide a deterministic and reproducible ML context
-        # - Float64
-        # - Boolean
-        # - Categorical
-        # - Datetime
+    @pa.dataframe_check
+    def coerce_data_types(cls, df: pd.DataFrame) -> bool:
+        """Coerce data types in-place post validation."""
 
         # Normalize datetime columns to microsecond resolution and drop time zone info
         cls.get_logger().debug("Coerce datetime types")
@@ -74,11 +68,12 @@ class DataModelBaseML(DataModelBase):
 
         # There should be no string columns, but all categorical
         cls.get_logger().debug("Coerce categorical types")
-        st = df.select_dtypes(include="string").columns
-        df = df.astype(pd.Series(pd.CategoricalDtype(), st))
+        st = df.select_dtypes("string").columns
+        df[st] = df[st].astype(pd.CategoricalDtype())
 
         # All numerics are promoted to float64 to prevent downstream type conversions
         cls.get_logger().debug("Coerce numeric types")
-        df = df.astype(pd.Series(pd.Float64Dtype(), df.select_dtypes("number").columns))
+        nt = df.select_dtypes("number").columns
+        df[nt] = df[nt].astype(pd.Float64Dtype())
 
-        return df
+        return True
